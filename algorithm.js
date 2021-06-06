f6c24b7daa0346df01482b6b6fbab7ece050d6fc
John Meo <75390945+Krakenmeister@users.noreply.github.com>
2021-06-06 02:59:01 -0700

    Added parameter validation

--- algorithm.js
+++ algorithm.js
@@ -1,11 +1,11 @@
 //test_space = set of objects to choose origin elements from (duplicates allowed)
 //test_null = a placeholder object -- can be anything that is not an element of test_space
-//test_length = length of the destination arrays to do tests on
 const test_space = ['1', 1, [22], -1, 22, 22]
 const test_null = 'dog'
-const test_length = 20
 
-//For large number of tests, comment out array logging
+//test_length = length of the destination arrays to do tests on
+//For large number of tests or long arrays, comment out array logging
+const test_length = 20
 const num_tests = 100
 
 
@@ -29,9 +29,19 @@ function vectorSum(v) {
 //The 0th element of origin array is a placeholder for values -- its value at the end is unimportant
 //Goal is to transform origin array so that last elements (all besides 0th element) exactly match destination array
 //However, the only way to modify origin array is by copying value from one index to another
-function test(origin, destination) {
-	origin[0] = test_null
-
+function transform (origin, destination) {
+	//Parameter validation
+	if ((origin[0] !== test_null) || origin.includes(test_null, 1)) {
+		console.log('Null value misused')
+		return false
+	}
+	for (var i=0; i<destination.length; i++) {
+		if (!origin.includes(destination[i], 1)) {
+			console.log('Can\'t copy ' + destination[i] + ' because it doesn\'t exist in origin array')
+			return false
+		}
+	}
+	
 	//Build an array of unique objects in origin array (rep_objs)
 	//Build an array of indices for representative elements of each unique object (rep_elms)
 	var rep_objs = []
@@ -55,7 +65,6 @@ function test(origin, destination) {
   	for (var i=0; i<rep_objs.length; i++) {
   		dependencies.push(0)
   	}
-
 	for (var i=0; i<rep_objs.length; i++) {
 		for (var j=0; j<rep_objs.length; j++) {
 			if ((i != j) && (destination[rep_elems[j]-1] === rep_objs[i])) {
@@ -116,7 +125,7 @@ for (var n=0; n<num_tests; n++) {
 	//Create origin and destination arrays randomly
 	//Destination array can only have elements that are included in origin array (since only action is copying)
 	var origin = [test_null]
-	while (origin.length < (test_length+1)) {
+	for (var i=0; i<test_length; i++) {
 		origin.push(test_space[Math.floor(Math.random()*test_space.length)])
 	}
 	var destination = []
@@ -127,7 +136,7 @@ for (var n=0; n<num_tests; n++) {
 	console.log("Destination: " + destination.join())
 
 	//Perform algorithm on arrays
-	if (!test(origin, destination)) {
+	if (!transform(origin, destination)) {
 		alltestsverified = false
 	}
 }
