73c23964bfdf68a3e78f826df513b7d7862006b9
John Meo <75390945+Krakenmeister@users.noreply.github.com>
2021-06-06 02:20:49 -0700

    Generalized element space
    Elements can be anything -- numbers, strings, arrays, doesn't matter

--- algorithm.js
+++ algorithm.js
@@ -1,8 +1,8 @@
-//test_space = set of elements to work from; numbers 1 through test_space
-//test_length = length of arrays to work on, should be >= test_space
-	//test_space could be abstracted a level further to any set of objects,
-	//but test() function requires using the set {1, 2, ..., test_space-1, test_space}
-const test_space = 10
+//test_space = set of objects to choose origin elements from (duplicates allowed)
+//test_null = a placeholder object -- can be anything that is not an element of test_space
+//test_length = length of the destination arrays to do tests on
+const test_space = ['1', 1, [22], -1, 22, 22]
+const test_null = 'dog'
 const test_length = 20
 
 //For large number of tests, comment out array logging
@@ -29,31 +29,36 @@ function vectorSum(v) {
 //The 0th element of origin array is a placeholder for values -- its value at the end is unimportant
 //Goal is to transform origin array so that last elements (all besides 0th element) exactly match destination array
 //However, the only way to modify origin array is by copying value from one index to another
-function test(origin, destination, set_space) {
-	origin[0] = -1
+function test(origin, destination) {
+	origin[0] = test_null
 
-	//Build an array of indices for representative elements
+	//Build an array of unique objects in origin array (rep_objs)
+	//Build an array of indices for representative elements of each unique object (rep_elms)
+	var rep_objs = []
 	var rep_elems = []
-	for (var i=0; i<set_space; i++) {
-  		rep_elems.push(origin.indexOf(i+1))
-  	}
+	for (var i=1; i<origin.length; i++) {
+		if (!rep_objs.includes(origin[i])) {
+			rep_objs.push(origin[i])
+			rep_elems.push(i)
+		}
+	}
 
 	//If not a representative element, copy the value you want from the corresponding representative element
-	for (var i=0; i<destination.length; i++) {
-		if (!rep_elems.includes(i+1)) {
-			origin = copy(origin, rep_elems[destination[i]-1], (i+1))
-	  	}
+	for (var i=1; i<origin.length; i++) {
+		if (!rep_elems.includes(i)) {
+			origin = copy(origin, rep_elems[rep_objs.indexOf(destination[i-1])], i)
+		}
 	}
 
 	//Dependencies counts which how many other elements desire to copy the element you represent
 	var dependencies = []
-  	for (var i=0; i<set_space; i++) {
+  	for (var i=0; i<rep_objs.length; i++) {
   		dependencies.push(0)
   	}
 
-	for (var i=0; i<set_space; i++) {
-		for (var j=0; j<set_space; j++) {
-			if ((i != j) && (destination[rep_elems[j]-1] == (i+1))) {
+	for (var i=0; i<rep_objs.length; i++) {
+		for (var j=0; j<rep_objs.length; j++) {
+			if ((i != j) && (destination[rep_elems[j]-1] === rep_objs[i])) {
 				dependencies[i]++
 			}
 		}
@@ -63,8 +68,8 @@ function test(origin, destination, set_space) {
 	while (vectorSum(dependencies) > 0) {
 		//Find the element with the least dependencies that hasn't yet reached its destination
 		var minIndex = -1
-		for (var i=0; i<set_space; i++) {
-			if (destination[rep_elems[i]-1] != origin[rep_elems[i]]) {
+		for (var i=0; i<rep_objs.length; i++) {
+			if (destination[rep_elems[i]-1] !== origin[rep_elems[i]]) {
 				if (minIndex == -1) {
 					minIndex = i
 				} else if (dependencies[i] < dependencies[minIndex]) {
@@ -73,23 +78,23 @@ function test(origin, destination, set_space) {
 			}
 		}
 		if (dependencies[minIndex] == 0) {
-			if (origin[0] == destination[rep_elems[minIndex]-1]) {
+			if (origin[0] === destination[rep_elems[minIndex]-1]) {
 				//If completing a dependency cycle, reset temporary slot so that other dependency cycles can be started
 				origin = copy(origin, 0, rep_elems[minIndex])
 				dependencies[cycleStart]--
 				cycleStart = -1
-				origin[0] = -1
+				origin[0] = test_null
 			} else {
 				//No other elements require this elements' information, so just copy the value you want
-				origin = copy(origin, rep_elems[destination[rep_elems[minIndex]-1]-1], rep_elems[minIndex])
-				dependencies[destination[rep_elems[minIndex]-1]-1]--
+				origin = copy(origin, rep_elems[rep_objs.indexOf(destination[rep_elems[minIndex]-1])], rep_elems[minIndex])
+				dependencies[rep_objs.indexOf(destination[rep_elems[minIndex]-1])]--
 			}
 		} else {
 			//If the minimum number of dependencies is 1, this means that only dependency cycles remain
 			//Pick one to begin with, and store its information in the temporary slot
 			origin = copy(origin, rep_elems[minIndex], 0)
-			origin = copy(origin, rep_elems[destination[rep_elems[minIndex]-1]-1], rep_elems[minIndex])
-			dependencies[destination[rep_elems[minIndex]-1]-1]--
+			origin = copy(origin, rep_elems[rep_objs.indexOf(destination[rep_elems[minIndex]-1])], rep_elems[minIndex])
+			dependencies[rep_objs.indexOf(destination[rep_elems[minIndex]-1])]--
 			cycleStart = minIndex
 		}
 	}
@@ -97,7 +102,7 @@ function test(origin, destination, set_space) {
 	//Check if origin array successfully transformed to match destination
 	var verified = true
 	for (var i=0; i<destination.length; i++) {
-		if (origin[i+1] != destination[i]) {
+		if (origin[i+1] !== destination[i]) {
 			verified = false
 		}
 	}
@@ -109,11 +114,10 @@ function test(origin, destination, set_space) {
 var alltestsverified = true
 for (var n=0; n<num_tests; n++) {
 	//Create origin and destination arrays randomly
-	//If strict array bounds checking is required, make sure that origin includes all of the numbers 1 through test_space
 	//Destination array can only have elements that are included in origin array (since only action is copying)
-	var origin = [-1]
+	var origin = [test_null]
 	while (origin.length < (test_length+1)) {
-		origin.push(Math.floor(Math.random()*test_space)+1)
+		origin.push(test_space[Math.floor(Math.random()*test_space.length)])
 	}
 	var destination = []
 	for (var i=0; i<test_length; i++) {
@@ -121,9 +125,9 @@ for (var n=0; n<num_tests; n++) {
 	}
 	console.log("Origin: " + origin.join())
 	console.log("Destination: " + destination.join())
-  
+
 	//Perform algorithm on arrays
-	if (!test(origin, destination, test_space)) {
+	if (!test(origin, destination)) {
 		alltestsverified = false
 	}
 }
